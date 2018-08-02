16247373dea41c9a5ccdb324f9493f1f77103395
Arthur Vimond <arthurvimond@gmail.com>
2018-08-02 11:46:35 +0200

    feat(OscletonSDK): add initialize() API

--- core/src/main/java/fr/arthurvimond/oscletonsdk/OscletonSDK.kt
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/OscletonSDK.kt
@@ -9,4 +9,24 @@ class OscletonSDK {
         val instance by lazy { OscletonSDK() }
     }
 
+    // Private properties
+
+    private var sdkInitialized = false
+
+    // Public methods
+
+    /**
+     * Initialize the SDK.
+     *
+     * This method must be called at application launch,
+     * usually in the custom Application's onCreate() method.
+     *
+     * @since 0.1
+     */
+    fun initialize() {
+
+
+        sdkInitialized = true
+    }
+
 }

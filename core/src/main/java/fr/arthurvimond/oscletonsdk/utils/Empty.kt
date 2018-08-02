4c64e66175b537ce188a128518a1225118c502d9
Arthur Vimond <arthurvimond@gmail.com>
2018-08-02 12:10:52 +0200

    feat(core): add Empty utils enum class

---
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/utils/Empty.kt
@@ -0,0 +1,9 @@
+package fr.arthurvimond.oscletonsdk.utils
+
+/**
+ * This class is used by Rx Subjects to trigger onNext() events without any
+ * particular value.
+ */
+enum class Empty {
+    VOID
+}

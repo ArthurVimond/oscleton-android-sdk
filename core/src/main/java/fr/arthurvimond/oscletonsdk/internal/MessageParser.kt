37999bb582da9d27f87bc5ca73cacd6efb14c699
Arthur Vimond <arthurvimond@gmail.com>
2018-08-03 22:54:22 +0200

    feat(core): create MessageParser class

---
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/internal/MessageParser.kt
@@ -0,0 +1,10 @@
+package fr.arthurvimond.oscletonsdk.internal
+
+/**
+ * Created by Arthur Vimond on 03/08/2018.
+ */
+internal class MessageParser internal constructor(val liveSetDataManager: LiveSetDataManager) {
+
+
+
+}

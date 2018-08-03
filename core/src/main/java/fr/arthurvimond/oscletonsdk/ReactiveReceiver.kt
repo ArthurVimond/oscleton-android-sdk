11d424ee96c78d86979b8f78233c6961ee4f4e48
Arthur Vimond <arthurvimond@gmail.com>
2018-08-03 17:10:49 +0200

    feat(core): create ReactiveReceiver class

---
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/ReactiveReceiver.kt
@@ -0,0 +1,18 @@
+package fr.arthurvimond.oscletonsdk
+
+import fr.arthurvimond.oscletonsdk.internal.LiveSetDataManager
+import io.reactivex.Observable
+
+/**
+ * ReactiveReceiver contains RxJava Observables reacting to the Live set data changes.
+ *
+ * If a reactive approach doesn't fit your needs, consider implementing [CallbackReceiver] instead.
+ *
+ * @since 0.1
+ * @see CallbackReceiver
+ */
+class ReactiveReceiver internal constructor(private val liveSetDataManager: LiveSetDataManager) {
+
+    val tempo: Observable<Float> = liveSetDataManager.tempo
+
+}

fdf1979652d65a817909c73b532b63bed5d78f57
Arthur Vimond <arthurvimond@gmail.com>
2018-08-03 22:54:51 +0200

    feat(core): create MessageManager class

---
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/internal/MessageManager.kt
@@ -0,0 +1,11 @@
+package fr.arthurvimond.oscletonsdk.internal
+
+/**
+ * Created by Arthur Vimond on 03/08/2018.
+ */
+internal class MessageManager internal constructor(private val oscManager: OSCManager,
+                                                   private val liveSetDataManager: LiveSetDataManager) {
+
+
+
+}

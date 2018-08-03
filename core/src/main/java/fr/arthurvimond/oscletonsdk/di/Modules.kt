e6bc580f2c2f7a8fcb7f5a51683c08415b1cc662
Arthur Vimond <arthurvimond@gmail.com>
2018-08-03 22:56:36 +0200

    feat(Modules): provide internal classes with koin

--- core/src/main/java/fr/arthurvimond/oscletonsdk/di/Modules.kt
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/di/Modules.kt
@@ -1,10 +1,12 @@
 package fr.arthurvimond.oscletonsdk.di
 
-import fr.arthurvimond.oscletonsdk.internal.AppLifecycleObserver
-import fr.arthurvimond.oscletonsdk.internal.LiveSetDataManager
+import fr.arthurvimond.oscletonsdk.internal.*
 import org.koin.dsl.module.applicationContext
 
 internal val oscletonSDKModule = applicationContext {
     bean { AppLifecycleObserver() }
+    bean { MessageManager(get(), get()) }
+    bean { OSCManager() }
+    bean { MessageParser(get()) }
     bean { LiveSetDataManager() }
 }

6e550935aeece809caf258cfc76d42161fb0d9ec
Arthur Vimond <arthurvimond@gmail.com>
2018-08-04 10:05:12 +0200

    feat(Modules): provide Controller and Receiver classes with koin

--- core/src/main/java/fr/arthurvimond/oscletonsdk/di/Modules.kt
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/di/Modules.kt
@@ -1,12 +1,20 @@
 package fr.arthurvimond.oscletonsdk.di
 
+import fr.arthurvimond.oscletonsdk.CallbackReceiver
+import fr.arthurvimond.oscletonsdk.Controller
+import fr.arthurvimond.oscletonsdk.ReactiveReceiver
+import fr.arthurvimond.oscletonsdk.Receiver
 import fr.arthurvimond.oscletonsdk.internal.*
 import org.koin.dsl.module.applicationContext
 
 internal val oscletonSDKModule = applicationContext {
-    bean { AppLifecycleObserver() }
-    bean { MessageManager(get(), get()) }
+    bean { Controller(get()) }
+    bean { Receiver() }
+    bean { ReactiveReceiver(get()) }
+    bean { CallbackReceiver(get()) }
+    bean { MessageManager(get(), get(), get()) }
     bean { OSCManager() }
     bean { MessageParser(get()) }
     bean { LiveSetDataManager() }
+    bean { AppLifecycleObserver() }
 }

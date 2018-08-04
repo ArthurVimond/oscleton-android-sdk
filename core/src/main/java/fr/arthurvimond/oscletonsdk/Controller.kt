f2e6f62d4cd6a0f631516cb46e0fecc5d6aada12
Arthur Vimond <arthurvimond@gmail.com>
2018-08-04 10:05:51 +0200

    feat(Controller): implement general and transport APIs

--- core/src/main/java/fr/arthurvimond/oscletonsdk/Controller.kt
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/Controller.kt
@@ -1,5 +1,7 @@
 package fr.arthurvimond.oscletonsdk
 
+import fr.arthurvimond.oscletonsdk.extensions.int
+import fr.arthurvimond.oscletonsdk.internal.LiveAPI
 import fr.arthurvimond.oscletonsdk.internal.MessageManager
 
 /**
@@ -9,6 +11,32 @@ import fr.arthurvimond.oscletonsdk.internal.MessageManager
  */
 class Controller internal constructor(private val messageManager: MessageManager) {
 
-    
+    fun play() {
+        messageManager.sendMessage(LiveAPI.play)
+    }
+
+    fun stop() {
+        messageManager.sendMessage(LiveAPI.stop)
+    }
+
+    fun undo() {
+        messageManager.sendMessage(LiveAPI.undo)
+    }
+
+    fun redo() {
+        messageManager.sendMessage(LiveAPI.redo)
+    }
+
+    fun setTempo(tempo: Float) {
+        messageManager.sendMessage(LiveAPI.tempo, listOf(tempo))
+    }
+
+    fun setOverdub(enabled: Boolean) {
+        messageManager.sendMessage(LiveAPI.overdub, listOf(enabled.int))
+    }
+
+    fun setMetronome(enabled: Boolean) {
+        messageManager.sendMessage(LiveAPI.metronome, listOf(enabled.int))
+    }
 
 }

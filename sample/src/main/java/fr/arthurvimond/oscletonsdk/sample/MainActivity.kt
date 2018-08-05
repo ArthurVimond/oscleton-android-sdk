bc226c9ba975209980d71a9fc6392b16dfe6eb96
Arthur Vimond <arthurvimond@gmail.com>
2018-08-05 21:37:00 +0200

    feat(MainActivity): implement data binding

--- sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/MainActivity.kt
+++ sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/MainActivity.kt
@@ -1,12 +1,21 @@
 package fr.arthurvimond.oscletonsdk.sample
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import fr.arthurvimond.oscletonsdk.sample.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // UI
+    private val binding: MainActivityBinding by lazy {
+        DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
+
 }

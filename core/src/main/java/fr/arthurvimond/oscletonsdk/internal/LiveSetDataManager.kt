fc56b616a45d0bcd76dbaca9f027a1ad4b61ccc6
Arthur Vimond <arthurvimond@gmail.com>
2018-08-06 23:26:08 +0200

    feat(LiveSetDataManager): implement tracks, devices, parameters MutableMaps

--- core/src/main/java/fr/arthurvimond/oscletonsdk/internal/LiveSetDataManager.kt
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/internal/LiveSetDataManager.kt
@@ -4,10 +4,16 @@ import com.illposed.osc.OSCMessage
 import fr.arthurvimond.oscletonsdk.extensions.float
 import fr.arthurvimond.oscletonsdk.extensions.int
 import fr.arthurvimond.oscletonsdk.extensions.string
+import fr.arthurvimond.oscletonsdk.models.Device
 import fr.arthurvimond.oscletonsdk.models.DeviceParameter
+import fr.arthurvimond.oscletonsdk.models.DeviceParameterIndices
+import fr.arthurvimond.oscletonsdk.models.Track
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.subjects.PublishSubject
 
-internal class LiveSetDataManager internal constructor(messageManager: MessageManager) {
+internal class LiveSetDataManager internal constructor(private val messageManager: MessageManager) {
 
     // Public properties
 
@@ -19,37 +25,72 @@ internal class LiveSetDataManager internal constructor(messageManager: MessageMa
 
 
     // Device parameters
-
-    val deviceParameter: Observable<DeviceParameter> = messageManager.oscMessage
-            .filter { it.address == LiveAPI.trackDeviceParam }
-            .map { mapToDeviceParameter(it) }
-
+    val deviceParameter: Observable<DeviceParameter>
+        get() = _deviceParameter
 
     // Private properties
 
-    // RxJava
-//    private val compositeDisposable = CompositeDisposable()
+    private val _deviceParameter: PublishSubject<DeviceParameter> = PublishSubject.create()
 
-    // Device parameters
-//    private val deviceParameters: MutableMap<Triple<Int, Int, Int>, DeviceParameter> = mutableMapOf()
+    // RxJava
+    private val compositeDisposable = CompositeDisposable()
+    
+    private val tracks: MutableMap<Int, Track> = mutableMapOf()
+    private val devices: MutableMap<Pair<Int, Int>, Device> = mutableMapOf()
+    private val deviceParameters: MutableMap<Triple<Int, Int, Int>, DeviceParameter> = mutableMapOf()
+
+    private val currentDeviceParameterIndices: PublishSubject<DeviceParameterIndices> = PublishSubject.create()
 
     init {
-//        observeProperties()
+        observeProperties()
     }
 
-//    private fun observeProperties() {
-//
-//        // Device parameters
-//        messageManager.oscMessage
-//                .filter { it.address == LiveAPI.trackDeviceParam }
-//                .map { mapToDeviceParameter(it) }
-//                .subscribe {
-//                    val triple = Triple(it.trackIndex, it.deviceIndex, it.paramIndex)
-//                    deviceParameters[triple] = it
-//                }.addTo(compositeDisposable)
-//
-//    }
+    private fun observeProperties() {
 
+        // Fill the maps on device parameters changes
+        messageManager.oscMessage
+                .filter { it.address == LiveAPI.trackDeviceParam }
+                .map { mapToDeviceParameter(it) }
+                .subscribe {
+
+                    // Track
+                    tracks[it.trackIndex] = Track(it.trackIndex)
+
+                    // Device
+                    val pair = Pair(it.trackIndex, it.deviceIndex)
+                    devices[pair] = Device(it.trackIndex, it.deviceIndex)
+
+                    // Device parameter
+                    val triple = Triple(it.trackIndex, it.deviceIndex, it.paramIndex)
+                    deviceParameters[triple] = it
+
+                }.addTo(compositeDisposable)
+
+        // Current device parameter indices
+        messageManager.oscMessage
+                .filter { it.address == LiveAPI.trackDeviceParam }
+                .map { mapToDeviceParameterIndices(it) }
+                .subscribe { currentDeviceParameterIndices.onNext(it) }
+                .addTo(compositeDisposable)
+
+        // Emit full device parameter info from map
+        currentDeviceParameterIndices
+                .subscribe {
+
+                    val triple = Triple(it.trackIndex, it.deviceIndex, it.paramIndex)
+                    val deviceParam = deviceParameters.getValue(triple)
+
+                    val deviceParameter = DeviceParameter(it.trackIndex,
+                            it.deviceIndex,
+                            it.paramIndex,
+                            name = deviceParam.name,
+                            value = deviceParam.value)
+
+                    _deviceParameter.onNext(deviceParameter)
+                }
+                .addTo(compositeDisposable)
+
+    }
 
     // Mapper functions
 
@@ -58,19 +99,27 @@ internal class LiveSetDataManager internal constructor(messageManager: MessageMa
         val trackIndex = oscMessage.arguments[0].int
         val deviceIndex = oscMessage.arguments[1].int
         val paramIndex = oscMessage.arguments[2].int
-        val value = oscMessage.arguments[3].float
-        val name = oscMessage.arguments[4].string
+        val name = oscMessage.arguments[3].string
+        val value = oscMessage.arguments[4].float
 
-        val deviceParameter = DeviceParameter(
+        return DeviceParameter(
                 paramIndex = paramIndex,
                 deviceIndex = deviceIndex,
                 trackIndex = trackIndex,
-                value = value,
-                name = name
-        )
-
-        return deviceParameter
+                name = name,
+                value = value)
     }
 
+    private fun mapToDeviceParameterIndices(oscMessage: OSCMessage): DeviceParameterIndices {
+
+        val trackIndex = oscMessage.arguments[0].int
+        val deviceIndex = oscMessage.arguments[1].int
+        val paramIndex = oscMessage.arguments[2].int
+
+        return DeviceParameterIndices(
+                trackIndex = trackIndex,
+                deviceIndex = deviceIndex,
+                paramIndex = paramIndex)
+    }
 
 }

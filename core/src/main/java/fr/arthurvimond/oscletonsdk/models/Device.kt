a9f6fab664a333c420605a320f2eddc3887de1ad
Arthur Vimond <arthurvimond@gmail.com>
2018-08-06 23:21:05 +0200

    feat(core): implement Device model

---
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/models/Device.kt
@@ -0,0 +1,16 @@
+package fr.arthurvimond.oscletonsdk.models
+
+/**
+ * Data class representing a Live device.
+ *
+ * @constructor
+ *
+ * @property trackIndex The track position in the Live set
+ * @property deviceIndex The device position in the track
+ * @property name The device name
+ *
+ * @since 0.1
+ */
+data class Device(var trackIndex: Int,
+                  var deviceIndex: Int,
+                  var name: String = "")

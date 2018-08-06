2ab07cd935bf93096e7625ae299f7288b778c8d3
Arthur Vimond <arthurvimond@gmail.com>
2018-08-06 18:39:57 +0200

    doc(OnTempoChangeListener): change @since API version

--- core/src/main/java/fr/arthurvimond/oscletonsdk/listeners/OnTempoChangeListener.java
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/listeners/OnTempoChangeListener.java
@@ -4,7 +4,7 @@ package fr.arthurvimond.oscletonsdk.listeners;
  * Interface definition for a callback to be invoked
  * when the live set tempo changes.
  *
- * @since 1.0
+ * @since 0.1
  */
 public interface OnTempoChangeListener {
     void onTempoChange(float tempo);

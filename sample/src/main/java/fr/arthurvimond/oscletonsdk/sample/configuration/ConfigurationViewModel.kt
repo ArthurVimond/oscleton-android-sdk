b64b7eb2f802d11b0644a7262eabbac36d5d0c85
Arthur Vimond <arthurvimond@gmail.com>
2018-08-07 15:40:17 +0200

    feat(sample): implement ConfigurationViewModel

---
+++ sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/configuration/ConfigurationViewModel.kt
@@ -0,0 +1,12 @@
+package fr.arthurvimond.oscletonsdk.sample.configuration
+
+import android.arch.lifecycle.ViewModel
+
+class ConfigurationViewModel : ViewModel() {
+
+
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}

c2fd16835cc710b718a8e8fd15f7a2234cd71753
Arthur Vimond <arthurvimond@gmail.com>
2018-08-07 15:38:53 +0200

    feat(sample): implement ReceiverActivity

---
+++ sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/receiver/ReceiverActivity.kt
@@ -0,0 +1,46 @@
+package fr.arthurvimond.oscletonsdk.sample.receiver
+
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import fr.arthurvimond.oscletonsdk.sample.R
+import fr.arthurvimond.oscletonsdk.sample.databinding.ReceiverActivityBinding
+
+class ReceiverActivity : AppCompatActivity() {
+
+    // UI
+    private val binding: ReceiverActivityBinding by lazy {
+        DataBindingUtil.setContentView<ReceiverActivityBinding>(this, R.layout.activity_receiver)
+    }
+
+    // Data
+    private val viewModel: ReceiverViewModel by lazy {
+        ViewModelProviders.of(this).get(ReceiverViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.viewModel = viewModel
+        binding.setLifecycleOwner(this)
+
+        // Display back arrow
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Set toolbar title
+        supportActionBar?.title = resources.getString(R.string.receiver)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+        // Back arrow
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}

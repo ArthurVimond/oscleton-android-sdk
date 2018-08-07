e0fa76eec6243c6c87626892cdd56ab41205cf58
Arthur Vimond <arthurvimond@gmail.com>
2018-08-07 15:41:57 +0200

    feat(MainActivity): implement activities navigation

--- sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/MainActivity.kt
+++ sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/MainActivity.kt
@@ -1,10 +1,15 @@
 package fr.arthurvimond.oscletonsdk.sample
 
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import fr.arthurvimond.oscletonsdk.sample.configuration.ConfigurationActivity
+import fr.arthurvimond.oscletonsdk.sample.controller.ControllerActivity
 import fr.arthurvimond.oscletonsdk.sample.databinding.MainActivityBinding
+import fr.arthurvimond.oscletonsdk.sample.receiver.ReceiverActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,8 +28,36 @@ class MainActivity : AppCompatActivity() {
         binding.viewModel = viewModel
 
         viewModel.configure()
-        viewModel.observeLiveEvents()
 
+        bindProperties()
+    }
+
+    private fun bindProperties() {
+
+        // Controller
+        controllerTextView.setOnClickListener { goToControllerActivity() }
+
+        // Receiver
+        receiverTextView.setOnClickListener { goToReceiverActivity() }
+
+        // Configuration
+        configurationTextView.setOnClickListener { goToConfigurationActivity() }
+
+    }
+
+    private fun goToControllerActivity() {
+        val intent = Intent(this, ControllerActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToReceiverActivity() {
+        val intent = Intent(this, ReceiverActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToConfigurationActivity() {
+        val intent = Intent(this, ConfigurationActivity::class.java)
+        startActivity(intent)
     }
 
 }

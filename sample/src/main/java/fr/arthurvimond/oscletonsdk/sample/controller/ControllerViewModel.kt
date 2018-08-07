93317ec26db38e9e49ffaa9f711a363e4a570f2f
Arthur Vimond <arthurvimond@gmail.com>
2018-08-07 15:37:19 +0200

    feat(sample): implement ControllerViewModel

---
+++ sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/controller/ControllerViewModel.kt
@@ -0,0 +1,12 @@
+package fr.arthurvimond.oscletonsdk.sample.controller
+
+import android.arch.lifecycle.ViewModel
+
+class ControllerViewModel : ViewModel() {
+
+
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}

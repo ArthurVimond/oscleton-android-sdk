b9309f58a5590c061281a424632f92a6b51ac40d
Arthur Vimond <arthurvimond@gmail.com>
2018-08-14 16:19:46 +0200

    feat(sample): implement play / stop API examples

--- sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/controller/ControllerViewModel.kt
+++ sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/controller/ControllerViewModel.kt
@@ -1,10 +1,17 @@
 package fr.arthurvimond.oscletonsdk.sample.controller
 
 import android.arch.lifecycle.ViewModel
+import fr.arthurvimond.oscletonsdk.OscletonSDK
 
 class ControllerViewModel : ViewModel() {
 
+    fun play() {
+        OscletonSDK.instance.controller.play()
+    }
 
+    fun stop() {
+        OscletonSDK.instance.controller.stop()
+    }
 
     override fun onCleared() {
         super.onCleared()

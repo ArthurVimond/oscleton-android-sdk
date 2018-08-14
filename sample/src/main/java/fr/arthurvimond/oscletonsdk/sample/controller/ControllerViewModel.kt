16061c1c256371ddf81e2ae261a94c1342fa61c3
Arthur Vimond <arthurvimond@gmail.com>
2018-08-14 16:41:32 +0200

    feat(sample): implement setMetronome API example

--- sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/controller/ControllerViewModel.kt
+++ sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/controller/ControllerViewModel.kt
@@ -2,6 +2,7 @@ package fr.arthurvimond.oscletonsdk.sample.controller
 
 import android.arch.lifecycle.ViewModel
 import fr.arthurvimond.oscletonsdk.OscletonSDK
+import fr.arthurvimond.oscletonsdk.sample.utils.Logger
 
 class ControllerViewModel : ViewModel() {
 
@@ -11,6 +12,10 @@ class ControllerViewModel : ViewModel() {
 
     fun stop() {
         OscletonSDK.instance.controller.stop()
+    }
+
+    fun setMetronome(enabled: Boolean) {
+        OscletonSDK.instance.controller.setMetronome(enabled)
     }
 
     override fun onCleared() {

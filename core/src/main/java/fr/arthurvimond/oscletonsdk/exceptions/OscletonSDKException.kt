8706b7caee502f9d4647a203e9c7408d1f2d73f2
Arthur Vimond <arthurvimond@gmail.com>
2018-08-14 23:50:33 +0200

    doc(core): add missing @since annotation

--- core/src/main/java/fr/arthurvimond/oscletonsdk/exceptions/OscletonSDKException.kt
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/exceptions/OscletonSDKException.kt
@@ -4,5 +4,6 @@ package fr.arthurvimond.oscletonsdk.exceptions
  * Generic [RuntimeException] specific to Oscleton SDK.
  *
  * @param message The message explaining the reason of the exception.
+ * @since 0.1
  */
 class OscletonSDKException internal constructor(override var message: String) : RuntimeException()
--- core/src/main/java/fr/arthurvimond/oscletonsdk/utils/Empty.kt
+++ core/src/main/java/fr/arthurvimond/oscletonsdk/utils/Empty.kt
@@ -3,6 +3,8 @@ package fr.arthurvimond.oscletonsdk.utils
 /**
  * This class is used by Rx Subjects to trigger onNext() events without any
  * particular value.
+ *
+ * @since 0.1
  */
 enum class Empty {
     VOID

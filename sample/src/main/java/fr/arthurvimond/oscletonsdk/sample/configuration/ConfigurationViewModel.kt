c7a238557192ebadcd8ba7c0b3b954ec025322b7
Arthur Vimond <arthurvimond@gmail.com>
2018-08-15 16:30:55 +0200

    feat(sample): implement setComputerIP() API example in Configuration

--- sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/configuration/ConfigurationViewModel.kt
+++ sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/configuration/ConfigurationViewModel.kt
@@ -1,10 +1,21 @@
 package fr.arthurvimond.oscletonsdk.sample.configuration
 
 import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+import fr.arthurvimond.oscletonsdk.OscletonSDK
 
 class ConfigurationViewModel : ViewModel() {
 
+    val computerIPAddress: ObservableField<String> = ObservableField("")
 
+    fun setComputerIPAddress() {
+        val computerIP = computerIPAddress.get()
+        computerIP?.let {
+            if (!it.isEmpty()) {
+                OscletonSDK.instance.config.setComputerIP(computerIP)
+            }
+        }
+    }
 
     override fun onCleared() {
         super.onCleared()

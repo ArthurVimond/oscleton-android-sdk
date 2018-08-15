862df64eb421de13f65f3c7fc7d5b6e57b4cf8a8
Arthur Vimond <arthurvimond@gmail.com>
2018-08-15 16:56:22 +0200

    feat(sample): implement live, script and sdk versions UI examples

--- sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/configuration/ConfigurationViewModel.kt
+++ sample/src/main/java/fr/arthurvimond/oscletonsdk/sample/configuration/ConfigurationViewModel.kt
@@ -1,12 +1,23 @@
 package fr.arthurvimond.oscletonsdk.sample.configuration
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.LiveDataReactiveStreams
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
 import fr.arthurvimond.oscletonsdk.OscletonSDK
+import io.reactivex.BackpressureStrategy
 
 class ConfigurationViewModel : ViewModel() {
 
     val computerIPAddress: ObservableField<String> = ObservableField("")
+
+    val liveVersion: LiveData<String> = LiveDataReactiveStreams.fromPublisher(
+            OscletonSDK.instance.config.liveVersion.toFlowable(BackpressureStrategy.LATEST))
+
+    val scriptVersion: LiveData<String> = LiveDataReactiveStreams.fromPublisher(
+            OscletonSDK.instance.config.scriptVersion.toFlowable(BackpressureStrategy.LATEST))
+
+    val sdkVersion: ObservableField<String> = ObservableField(OscletonSDK.instance.config.sdkVersion)
 
     fun setComputerIPAddress() {
         val computerIP = computerIPAddress.get()

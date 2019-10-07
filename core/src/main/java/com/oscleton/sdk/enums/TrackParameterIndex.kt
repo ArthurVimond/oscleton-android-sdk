6c2cc3313171b1cc6e2df0d28626479630ca4185
Arthur Vimond <arthurvimond@gmail.com>
2019-10-07 21:10:41 +0200

    feat(enums): create TrackParameterIndex constants

---
+++ core/src/main/java/com/oscleton/sdk/enums/TrackParameterIndex.kt
@@ -0,0 +1,14 @@
+package com.oscleton.sdk.enums
+
+/**
+ * Enum class representing a Live track parameter index.
+ *
+ * @since 0.4
+ */
+internal object TrackParameterIndex {
+    const val VOLUME = 0
+    const val PANNING = 1
+    const val MUTE = 2
+    const val SOLO = 3
+    const val ARM = 4
+}

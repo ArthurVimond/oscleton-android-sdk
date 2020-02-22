a2f89ef0ed2f59357472a72010998ba9fe4faf82
Arthur Vimond <arthurvimond@gmail.com>
2020-02-22 09:37:05 +0100

    feat(NetworkUtils): implement isConnected() method

--- core/src/main/java/com/oscleton/sdk/utils/NetworkUtils.kt
+++ core/src/main/java/com/oscleton/sdk/utils/NetworkUtils.kt
@@ -1,5 +1,8 @@
 package com.oscleton.sdk.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import java.net.NetworkInterface
 import java.util.*
 
@@ -44,6 +47,12 @@ object NetworkUtils {
         }
 
         return ""
+    }
+
+    fun isConnected(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnected == true
     }
 
 }

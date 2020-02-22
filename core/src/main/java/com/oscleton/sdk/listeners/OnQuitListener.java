042e447b07e26df296c88e3597bdb738895c9fe5
Arthur Vimond <arthurvimond@gmail.com>
2020-02-22 09:31:20 +0100

    feat(listeners): create OnQuitListener interface

---
+++ core/src/main/java/com/oscleton/sdk/listeners/OnQuitListener.java
@@ -0,0 +1,11 @@
+package com.oscleton.sdk.listeners;
+
+/**
+ * Interface definition for a callback to be invoked
+ * when Ableton Live quits.
+ *
+ * @since 0.6
+ */
+public interface OnQuitListener {
+    void onQuit();
+}

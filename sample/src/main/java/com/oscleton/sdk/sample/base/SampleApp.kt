1807cb0c54aebc4b3e6a39c55e7ad7941dac95a6
Arthur Vimond <arthurvimond@gmail.com>
2020-02-22 10:13:42 +0100

    feat(sample): start Koin on app creation

--- sample/src/main/java/com/oscleton/sdk/sample/base/SampleApp.kt
+++ sample/src/main/java/com/oscleton/sdk/sample/base/SampleApp.kt
@@ -2,11 +2,17 @@ package com.oscleton.sdk.sample.base
 
 import android.app.Application
 import com.oscleton.sdk.OscletonSDK
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class SampleApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            androidContext(this@SampleApp)
+        }
 
         // Initialize SDK
         OscletonSDK.instance.initialize()

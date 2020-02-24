f8db104828440051615a9a8d4b1972d122d8e25a
Arthur Vimond <arthurvimond@gmail.com>
2020-02-24 19:15:18 +0100

    feat(enums): create MasterParameterIndex

---
+++ core/src/main/java/com/oscleton/sdk/enums/MasterParameterIndex.kt
@@ -0,0 +1,12 @@
+package com.oscleton.sdk.enums
+
+/**
+ * Constants class representing a Live master parameter index.
+ *
+ * @since 0.7
+ */
+internal object MasterParameterIndex {
+    const val VOLUME = 0
+    const val PANNING = 1
+    const val SOLO = 2
+}

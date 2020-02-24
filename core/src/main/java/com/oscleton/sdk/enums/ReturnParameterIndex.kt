a12f9479d98594f3251ee47dd1deda278db03bd0
Arthur Vimond <arthurvimond@gmail.com>
2020-02-24 19:08:26 +0100

    feat(enums): create ReturnParameterIndex

---
+++ core/src/main/java/com/oscleton/sdk/enums/ReturnParameterIndex.kt
@@ -0,0 +1,13 @@
+package com.oscleton.sdk.enums
+
+/**
+ * Constants class representing a Live return parameter index.
+ *
+ * @since 0.7
+ */
+internal object ReturnParameterIndex {
+    const val VOLUME = 0
+    const val PANNING = 1
+    const val MUTE = 2
+    const val SOLO = 3
+}

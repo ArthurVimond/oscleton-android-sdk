1a34234fa531e6ec0805c0743c28cfdbc10c5c6b
Arthur Vimond <arthurvimond@gmail.com>
2020-12-04 22:46:28 +0100

    feat(core-rxjava2): implement Koin module

---
+++ core-rxjava2/src/main/java/com/oscleton/sdk/rx/di/Modules.kt
@@ -0,0 +1,12 @@
+package com.oscleton.sdk.rx.di
+
+import com.oscleton.sdk.rx.ConfigurationRx
+import com.oscleton.sdk.rx.LiveSetRx
+import com.oscleton.sdk.rx.TracksRx
+import org.koin.dsl.module
+
+internal val reactiveModule = module {
+    single { ConfigurationRx(get()) }
+    single { LiveSetRx(get()) }
+    single { TracksRx(get()) }
+}

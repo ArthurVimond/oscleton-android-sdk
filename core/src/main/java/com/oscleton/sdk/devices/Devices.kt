5c3add22c5a1bb097cda0bfb31fbdc57fdb6db29
Arthur Vimond <arthurvimond@gmail.com>
2020-12-04 22:19:31 +0100

    feat(core): implement Devices

---
+++ core/src/main/java/com/oscleton/sdk/devices/Devices.kt
@@ -0,0 +1,10 @@
+package com.oscleton.sdk.devices
+
+import com.oscleton.sdk.internal.MessageManager
+
+class Devices internal constructor(private val devicesDataManager: DevicesDataManager,
+                                  private val messageManager: MessageManager) {
+
+
+
+}

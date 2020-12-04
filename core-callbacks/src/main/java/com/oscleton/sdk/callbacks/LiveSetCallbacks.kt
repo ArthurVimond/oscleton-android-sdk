cead985f736e4f3ec3ccec67a80f6302bbba5349
Arthur Vimond <arthurvimond@gmail.com>
2020-12-04 22:48:09 +0100

    feat(core-callbacks): implement LiveSetCallbacks

---
+++ core-callbacks/src/main/java/com/oscleton/sdk/callbacks/LiveSetCallbacks.kt
@@ -0,0 +1,47 @@
+package com.oscleton.sdk.callbacks
+
+import com.oscleton.sdk.liveset.LiveSetDataManager
+import com.oscleton.sdk.callbacks.listeners.OnTempoChangeListener
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+class LiveSetCallbacks internal constructor(private val liveSetDataManager: LiveSetDataManager) {
+
+    // Rx
+    private val compositeDisposable = CompositeDisposable()
+    private var tempoDisp: Disposable? = null
+
+    /**
+     * Register a callback to be invoked when the general tempo changes.
+     *
+     * @param listener The callback that will run
+     * @since 1.0
+     */
+    fun set(listener: OnTempoChangeListener) {
+        tempoDisp = liveSetDataManager.tempo
+                .subscribe {
+                    listener.onTempoChange(it)
+                }
+
+        compositeDisposable.add(tempoDisp!!)
+    }
+
+    /**
+     * Remove the current [OnTempoChangeListener].
+     *
+     * @since 1.0
+     */
+    fun removeOnTempoChangeListener() {
+        tempoDisp?.dispose()
+    }
+
+    /**
+     * Remove all listeners.
+     *
+     * @since 1.0
+     */
+    fun removeAllListeners() {
+        compositeDisposable.clear()
+    }
+
+}

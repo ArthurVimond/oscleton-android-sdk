2a3c5ce1285a5febb1722ba80df429319796acd6
Arthur Vimond <arthurvimond@gmail.com>
2020-12-04 22:15:07 +0100

    feat(core): implement Tracks

---
+++ core/src/main/java/com/oscleton/sdk/tracks/Tracks.kt
@@ -0,0 +1,10 @@
+package com.oscleton.sdk.tracks
+
+import com.oscleton.sdk.internal.MessageManager
+
+class Tracks internal constructor(private val tracksDataManager: TracksDataManager,
+                                  private val messageManager: MessageManager) {
+
+
+
+}

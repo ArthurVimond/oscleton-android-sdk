206a4cce38e0253130bd68bee98f9b8df163e404
Arthur Vimond <arthurvimond@gmail.com>
2020-12-17 15:39:42 +0100

    fix(core-rxjava2): add missing DevicesRx definition

--- core-rxjava2/src/main/java/com/oscleton/sdk/rx/di/Modules.kt
+++ core-rxjava2/src/main/java/com/oscleton/sdk/rx/di/Modules.kt
@@ -1,6 +1,7 @@
 package com.oscleton.sdk.rx.di
 
 import com.oscleton.sdk.rx.ConfigurationRx
+import com.oscleton.sdk.rx.DevicesRx
 import com.oscleton.sdk.rx.LiveSetRx
 import com.oscleton.sdk.rx.TracksRx
 import org.koin.dsl.module
@@ -9,4 +10,5 @@ internal val reactiveModule = module {
     single { ConfigurationRx(get()) }
     single { LiveSetRx(get()) }
     single { TracksRx(get()) }
+    single { DevicesRx(get()) }
 }

34bb8a06c7073b4b9c9ea5504e628fa9586a3acc
Arthur Vimond <arthurvimond@gmail.com>
2022-05-07 14:52:12 +0200

    feat(TrackParameterIndex): change visibility to public

--- core/src/main/java/com/oscleton/sdk/enums/TrackParameterIndex.kt
+++ core/src/main/java/com/oscleton/sdk/enums/TrackParameterIndex.kt
@@ -5,7 +5,7 @@ package com.oscleton.sdk.enums
  *
  * @since 0.4
  */
-internal object TrackParameterIndex {
+object TrackParameterIndex {
     const val VOLUME = 0
     const val PANNING = 1
     const val MUTE = 2

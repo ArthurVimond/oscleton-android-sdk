cedf1e9c2d32e2f6c464396392432743dc6712ba
Arthur Vimond <arthurvimond@gmail.com>
2022-05-07 14:51:02 +0200

    feat(Devices): implement device parameter setter methods

--- core/src/main/java/com/oscleton/sdk/devices/Devices.kt
+++ core/src/main/java/com/oscleton/sdk/devices/Devices.kt
@@ -1,5 +1,6 @@
 package com.oscleton.sdk.devices
 
+import com.oscleton.sdk.internal.LiveAPI
 import com.oscleton.sdk.internal.MessageManager
 
 /**
@@ -10,6 +11,42 @@ import com.oscleton.sdk.internal.MessageManager
 class Devices internal constructor(private val devicesDataManager: DevicesDataManager,
                                   private val messageManager: MessageManager) {
 
+    /**
+     * Set a track device parameter value.
+     *
+     * @param trackIndex the track index
+     * @param deviceIndex the device index
+     * @param paramIndex the parameter index
+     * @param value the parameter value to set
+     * @since 1.1
+     */
+    fun setTrackDeviceParameter(trackIndex: Int, deviceIndex: Int, paramIndex: Int, value: Float) {
+        messageManager.sendMessage(LiveAPI.trackDeviceParam, listOf(trackIndex, deviceIndex, paramIndex, value))
+    }
 
+    /**
+     * Set a return track device parameter value.
+     *
+     * @param trackIndex the return track index
+     * @param deviceIndex the device index
+     * @param paramIndex the parameter index
+     * @param value the parameter value to set
+     * @since 1.1
+     */
+    fun setReturnDeviceParameter(trackIndex: Int, deviceIndex: Int, paramIndex: Int, value: Float) {
+        messageManager.sendMessage(LiveAPI.returnDeviceParam, listOf(trackIndex, deviceIndex, paramIndex, value))
+    }
+
+    /**
+     * Set a master track device parameter value.
+     *
+     * @param deviceIndex the device index
+     * @param paramIndex the parameter index
+     * @param value the parameter value to set
+     * @since 1.1
+     */
+    fun setMasterDeviceParameter(deviceIndex: Int, paramIndex: Int, value: Float) {
+        messageManager.sendMessage(LiveAPI.masterDeviceParam, listOf(0, deviceIndex, paramIndex, value))
+    }
 
 }
